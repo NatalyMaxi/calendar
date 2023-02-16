84956e9cb55909fe03310376cec393ac3de1e848
Natalya Maximova <natka-31@mail.ru>
2023-02-17 01:34:59 +0500

    Реализовать отображения дней компонента DaysGrid

--- src/components/DayCell/DayCell.jsx
+++ src/components/DayCell/DayCell.jsx
@@ -4,12 +4,12 @@ const DayCell = (props) => {
    return (
       <section className='day-element'>
          <div className='day-element__data-container'>
-            <span className='day-element__day-week'>Понедельник, </span>
-            <span className='day-element__number'>1</span>
+            <span className='day-element__day-week'>{props.dayWeek}</span>
+            <span className='day-element__number'>{props.num}</span>
          </div>
          <div className='day-element__info-container'>
-            <p className='day-element__title'>Шашлыки</p>
-            <p className='day-element__text'>Филип Коров, Дмитрий Табасков</p>
+            <p className='day-element__title'></p>
+            <p className='day-element__text'></p>
          </div>
       </section>
    );
--- src/components/DaysGrid/DaysGrid.jsx
+++ src/components/DaysGrid/DaysGrid.jsx
@@ -1,10 +1,27 @@
+import dateFns from "date-fns";
 import DayCell from '../DayCell/DayCell';
 import './DaysGrid.css';
 
 const DaysGrid = (props) => {
+   const monthStart = dateFns.startOfMonth(props.currentDate);
+   const startDate = dateFns.startOfWeek(monthStart);
+   const arrayDaysWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье',]
+   const daysArray = [...Array(42)].map((el, i) => dateFns.addDays(startDate, i + 1))
+   const newDaysArray = Object(daysArray)
+
    return (
       <section className='content'>
-         <DayCell />
+         {
+            newDaysArray.map((el, index) => {
+               let formattedDate = dateFns.format(el, "D")
+               if (index < 7) {
+                  // let dayWeek = (dateFns.format(dateFns.addDays(firstDayOfWeek, index +1), "dddd")) //английский язык
+                  return <DayCell num={formattedDate} dayWeek={arrayDaysWeek[index] + ', '} key={index} />
+                  // return <DayCell num={formattedDate} dayWeek={dayWeek + ', '} key={index} /> //английский язык
+               }
+               return <DayCell num={formattedDate} dayWeek={''} key={index} />
+            })
+         }
       </section>
    );
 };
