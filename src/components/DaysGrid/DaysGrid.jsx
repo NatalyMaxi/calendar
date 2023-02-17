1cb2a23e403b6c4d8422e66db12b69ea578c5837
Natalya Maximova <natka-31@mail.ru>
2023-02-17 12:39:34 +0500

    Вынести импорт функций библиотеки date-fns

--- src/components/DaysGrid/DaysGrid.jsx
+++ src/components/DaysGrid/DaysGrid.jsx
@@ -1,21 +1,21 @@
-import dateFns from 'date-fns';
+import { format, addDays, startOfMonth, startOfWeek } from 'date-fns';
 import DayCell from '../DayCell/DayCell';
 import './DaysGrid.css';
 
 const DaysGrid = (props) => {
-   const monthStart = dateFns.startOfMonth(props.currentDate);
-   const startDate = dateFns.startOfWeek(monthStart);
+   const monthStart = startOfMonth(props.currentDate);
+   const startDate = startOfWeek(monthStart);
    const arrayDaysWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье',]
-   const daysArray = [...Array(42)].map((el, i) => dateFns.addDays(startDate, i + 1))
+   const daysArray = [...Array(42)].map((el, i) => addDays(startDate, i + 1))
    const newDaysArray = Object(daysArray)
 
    return (
       <section className='content'>
          {
             newDaysArray.map((el, index) => {
-               let formattedDate = dateFns.format(el, "D")
+               let formattedDate = format(el, "D")
                if (index < 7) {
-                  // let dayWeek = (dateFns.format(dateFns.addDays(firstDayOfWeek, index +1), "dddd")) //английский язык
+                  // let dayWeek = (format(dateFns.addDays(firstDayOfWeek, index +1), "dddd")) //английский язык
                   return <DayCell num={formattedDate} dayWeek={arrayDaysWeek[index] + ', '} key={index} />
                   // return <DayCell num={formattedDate} dayWeek={dayWeek + ', '} key={index} /> //английский язык
                }
